70841889f69e19933df5e2ed2fa225558481f5dc
Jaume Sanchez <the.spite@gmail.com>
2016-03-14 23:40:55 +0100

    updated build

--- utils/build.sh
+++ utils/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 java -jar compiler.jar --js=../src/CCapture.js --js_output_file=../build/CCapture.min.js
+java -jar compiler.jar --js=../src/download.js --js=../src/Whammy.js --js=../src/tar.js --js=../src/gif.js --js=../src/CCapture.js --js_output_file=../build/CCapture.all.min.js
