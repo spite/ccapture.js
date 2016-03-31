4e36432a13dd8d52b1d36c0ccf6a764c4592297e
Jaume Sanchez <the.spite@gmail.com>
2016-03-31 10:21:43 +0200

    added initial support for HTMLCanvasElement.captureStrem -> MediaRecorder

--- src/CCapture.js
+++ src/CCapture.js
@@ -322,6 +322,53 @@ CCFFMpegServerEncoder.prototype.safeToProceed = function() {
     return this.encoder.safeToProceed();
 };
 
+/*
+	HTMLCanvasElement.captureStream()
+*/
+
+function CCStreamEncoder( settings ) {
+
+	CCFrameEncoder.call( this, settings );
+
+	this.framerate = this.settings.framerate;
+	this.type = 'video/webm';
+	this.fileExtension = '.webm';
+	this.stream = null;
+	this.mediaRecorder = null;
+	this.chunks = [];
+
+}
+
+CCStreamEncoder.prototype = Object.create( CCFrameEncoder.prototype );
+
+CCStreamEncoder.prototype.add = function( canvas ) {
+
+	if( !this.stream ) {
+		this.stream = canvas.captureStream( this.framerate );
+		this.mediaRecorder = new MediaRecorder( this.stream );
+		this.mediaRecorder.start();
+
+		this.mediaRecorder.ondataavailable = function(e) {
+			this.chunks.push(e.data);
+		}.bind( this );
+
+	}
+	this.step();
+
+}
+
+CCStreamEncoder.prototype.save = function( callback ) {
+
+	this.mediaRecorder.onstop = function( e ) {
+		var blob = new Blob( this.chunks, { 'type' : 'video/webm' });
+		this.chunks = [];
+		callback( blob );
+
+	}.bind( this );
+
+	this.mediaRecorder.stop();
+	
+}
 
 /*function CCGIFEncoder( settings ) {
 
@@ -507,7 +554,8 @@ function CCapture( settings ) {
 		webm: CCWebMEncoder,
 		ffmpegserver: CCFFMpegServerEncoder,
 		png: CCPNGEncoder,
-		jpg: CCJPEGEncoder
+		jpg: CCJPEGEncoder,
+		stream: CCStreamEncoder
     };
 
     var ctor = _encoders[ _settings.format ];
