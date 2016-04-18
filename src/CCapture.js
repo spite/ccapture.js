73f4b17a1420326395f271197ddd2c7caf6d336d
Jaume Sanchez <the.spite@gmail.com>
2016-04-18 13:01:48 +0200

    fixed settings.display

--- src/CCapture.js
+++ src/CCapture.js
@@ -540,7 +540,7 @@ function CCapture( settings ) {
 	_timeDisplay.style.padding = '5px'
 	_timeDisplay.style.color = 'red';
 	_timeDisplay.style.zIndex = 100000
-	document.body.appendChild( _timeDisplay );
+	if( _settings.display ) document.body.appendChild( _timeDisplay );
 
 	var canvasMotionBlur = document.createElement( 'canvas' );
 	var ctxMotionBlur = canvasMotionBlur.getContext( '2d' );
