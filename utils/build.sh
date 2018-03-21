3aa198661d40f3d444ef0452aec639a32f5b4ea4
Jaume Sanchez <the.spite@gmail.com>
2018-03-21 10:10:08 +0000

    disabled minifying for whammy

--- utils/build.sh
+++ utils/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 uglifyjs ../src/CCapture.js --compress --mangle -o ../build/CCapture.min.js
-uglifyjs ../src/download.js ../src/Whammy.js ../src/tar.js ../src/gif.js ../src/CCapture.js --compress --mangle -o ../build/CCapture.all.min.js
-
+uglifyjs ../src/download.js ../src/tar.js ../src/gif.js ../src/CCapture.js --compress --mangle -o ../build/CCapture.all.min.js
+cat ../src/Whammy.js >> ../build/CCapture.all.min.js
